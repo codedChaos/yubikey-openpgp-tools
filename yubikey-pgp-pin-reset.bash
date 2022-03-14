ed1255f2e35914f43945df69665c31ada8a212f8
codedChaos <devopsmyth@gmail.com>
2022-03-14 02:02:50 -0400

    Init(yubikey-gpg-pin-reset): reset PIN
    Shell script to "cash" / exhaust all PIN retries left on Yubikey card
    After retries are reduced to 0 for card PIN, reset PIN to default Yubikey PINs
    User PIN default: 123456
    Admin PIN default: 12345678
    Integrating `shflags` library for command-line options parsing
    First commit of file
    Begin writing code for handling command-line options
    
    #untested  #work-in-progress

---
+++ yubikey-pgp-pin-reset.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Yubikey OpenPGP/GnuPG Card - PIN Reset script
+#
+# Source shflags
+source ./lib/shflags/shflags
+
+# Define command-line args
+DEFINE_boolean 'debug' false 'enable debug mode' 'd'
+DEFINE_boolean 'admin-pin' false 'enable reset of admin PIN' 'a'
+DEFINE_boolean 'pin' false 'enable reset of user PIN' 'p'
+DEFINE_boolean 'all' false 'enable reset of user and admin PINs'
+DEFINE_
+FLAGS_HELP=$(cat <<EOF
+USAGE: $0 [flags] command
+commands:
+    list:       show cards visible to gpg
+    verify:     verify working PIN
+    backup:     backup current PIN
+    reset:      reset PIN
+EOF
+)
+
+
+debug() {
+    # code for debug mode
+    [ ${FLAGS_debug} -eq ${FLAGS_TRUE} ] && echo "DEBUG: $@" >$2
+}
+
+main() {
+    # main script function
+    
+}
+
+# parse command-line options
+FLAGS "$@" || exit $?
+eval set -- "${FLAGS_ARGV}"
+main "$@"
